b7f6f2b69249add64c6f30e906b546e8e2664dbe
Dhayasanjai <66767492+Dhayasanjai@users.noreply.github.com>
2021-06-14 11:47:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,26 @@ a.addEventListener('click', () => {
   backdrop.style.display = 'none';
   nav.style.display = 'none';
 });
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+});
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == 'P'.charCodeAt(0)) {
+    return false;
+  }
+};
